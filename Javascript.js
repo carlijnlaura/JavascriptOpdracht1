b5961ef5dbe89c9ebfab873ee77ebc4a525d4f3c
Carlijn <carlijn.denhartog@student.rocvf.nl>
2017-12-11 10:36:37 +0100

    Opdracht 1.2 en 1.3

---
+++ Javascript.js
@@ -0,0 +1,17 @@
+// Opdracht 1.2
+for (var i= 0; i<11; i++) {
+
+    console.log("De waarde van i is " + i);
+}
+
+//Opdracht 1.3
+var mijnauto = {
+    merk:"Ford",
+    type:"Mondeo",
+    aantalWielen: 4,
+    kleur: "blauw"
+}
+
+    console.log(mijnauto.kleur)
+
+
