ee08618ee25867a3c66a39041bc397d17fec813d
carlijnlaura <31473457+carlijnlaura@users.noreply.github.com>
2017-12-13 09:38:49 +0100

    Delete Opdracht2.1.js

--- Opdracht2.1.js
+++
@@ -1,7 +0,0 @@
-
-
-document.getElementById("paginatitel").innerText = "Dit is de paginatitel";
-document.getElementById("subkop").innerText = "Dit is de subkop";
-
-document.getElementById("vakantiefoto1").src= "img/Photo 2. copy.JPG";
-document.getElementById("vakantiefoto2").src= "img/Photo 6. copy.JPG";
